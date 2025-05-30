74ee0705bb6b2e498b77fe64f08271295c03cde5
Rajendra1910 <rajendra.p1910@gmail.com>
2025-05-30 15:33:32 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,7 @@
-variable "region" {
-  default = "us-east-1"
-}
+variable "region" {us-east-1}
+variable "project_name" {devops-hackathon}
+variable "vpc_cidr" {10.0.0.0/16}
+variable "public_subnet_az1_cidr" {10.0.1.0/24}
+variable "public_subnet_az2_cidr" {10.0.3.0/24}
+variable "private_subnet_az1_cidr" {10.0.2.0/24}
+variable "private_subnet_az2_cidr" {10.0.4.0/24}
