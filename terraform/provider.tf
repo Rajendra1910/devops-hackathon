7eb8984942dc9ea8b8ca00b94913974219326a04
Rajendra1910 <rajendra.p1910@gmail.com>
2025-05-30 16:12:24 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIAX3NVJFI7HS6IMLU6"
-  secret_key = "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"
+region = "${var.AWS_REGION}"
+profile = "${var.AWS_PROFILE}" 
 }
