4149290ea3212aa9907dee8afd376ccf10120e96
Rajendra1910 <rajendra.p1910@gmail.com>
2025-05-30 16:19:33 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,5 @@ variable "public_subnet_az1_cidr" {10.0.1.0/24}
 variable "public_subnet_az2_cidr" {10.0.3.0/24}
 variable "private_subnet_az1_cidr" {10.0.2.0/24}
 variable "private_subnet_az2_cidr" {10.0.4.0/24}
+variable "Access key" {AKIAX3NVJFI7HS6IMLU6}
+variable "secret key" {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}
